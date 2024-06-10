04b71a1c36f89c8cc2bac6577d771e2b90bf3464
Lumina <ipa22.a.zibrevs@vtdt.edu.lv>
2024-06-10 10:14:33 +0300

    Summary, tried installing vite

--- HotelCoop/vite.config.js
+++ HotelCoop/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [
@@ -8,4 +9,6 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    plugins: [react()],
+
 });
